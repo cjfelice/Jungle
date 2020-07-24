2124758202d3b99a384382550f6a4da3c9c9e983
cjfelice <monocularmedia@gmail.com>
2020-07-24 19:44:13 +0000

    added admin page functionality

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: "Jungle", password: "book"
 
   def show
+    @categoryCount = Category.count
+    @productCount = Product.count
   end
 end
